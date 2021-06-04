570d0c7f0e3c7b42684245e7c0d125233732dddf
Albert Likaj <dagobah1528@gmail.com>
2021-06-05 00:20:00 +0200

    Update

--- windows.el
+++ windows.el
@@ -4,6 +4,9 @@
 (setenv "JAVA_HOME"  "C:/Program Files/Java/jdk-16.0.1")
 (setq lsp-java-java-path "C:/Program Files/Java/jdk-16.0.1/bin/java.exe")
 
-; add powershell to exec-path
+; add powershell and git to exec-path
+; powershell must be set in exec-path otherwise lsp-java can not
+; install the lsp server
 (add-to-list 'exec-path "C:/Windows/system32/WindowsPowerShell/v1.0")
+(add-to-list 'exec-path "C:/Program Files/Git/bin")
 (setenv "PATH" (mapconcat #'identity exec-path path-separator))
