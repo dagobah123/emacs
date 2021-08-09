a4bfe003a901406285cf21964c990a15084967b1
Albert Likaj <dagobah1528@gmail.com>
2021-08-09 08:20:43 +0200

    Modify mode-line font

--- chiaroscuro-theme.el
+++ chiaroscuro-theme.el
@@ -195,6 +195,8 @@
     `(ivy-current-match ((,class (:foreground ,white-yellow :bold t))))
     `(eww-valid-certificate ((,class (:foreground ,orange :bold t))))
 
+    ;`(variable-pitch ((,class (:foreground ,orange :bold t))))
+
     ;(set-frame-font "DejaVu Sans Mono-30" nil t)
     ;(set-frame-font "STIXGeneral-20" nil t)
     ;(set-frame-font "FreeSerif-20" nil t)
