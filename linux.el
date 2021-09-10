ff89059fce36b54a5919e2ac83b8126fc09c9400
Albert Likaj <dagobah1528@gmail.com>
2021-09-10 22:36:34 +0200

    Update

--- linux.el
+++ linux.el
@@ -3,32 +3,32 @@
 ; (setenv "JAVA_HOME" "/usr/lib/jvm/java-14-openjdk-amd64")
 
 ; set font of mode-line
-(set-face-attribute 'mode-line nil :font "Comfortaa-25")
+(set-face-attribute 'mode-line nil :font "Comfortaa-20")
 
 (custom-theme-set-faces
  'user
-  '(org-table ((t (:family "DejaVu Sans Mono" :height 250))))
-  '(org-link ((t (:family "DejaVu Sans Mono" :height 250 :underline t))))
+  '(org-table ((t (:family "DejaVu Sans Mono" :height 220))))
+  '(org-link ((t (:family "DejaVu Sans Mono" :height 220 :underline t))))
 )
 
 (defun my:buffer-face-mode-mono()
   (interactive)
-  (setq buffer-face-mode-face '(:family "FreeMono" :height 250 :weight bold))
+  (setq buffer-face-mode-face '(:family "FreeMono" :height 220 :weight bold))
   (buffer-face-mode))
 
 (defun my:buffer-face-mode-text()
   (interactive)
-  (setq buffer-face-mode-face '(:family "FreeSerif" :height 250))
+  (setq buffer-face-mode-face '(:family "FreeSerif" :height 220))
   (buffer-face-mode))
 
 (defun my:buffer-face-mode-default()
   (interactive)
-  (setq buffer-face-mode-face '(:family "DejaVu Sans Mono" :height 250))
+  (setq buffer-face-mode-face '(:family "DejaVu Sans Mono" :height 220))
   (buffer-face-mode))
 
 (defun my:buffer-face-mode-stylish()
   (interactive)
-  (setq buffer-face-mode-face '(:family "Comfortaa" :height 250))
+  (setq buffer-face-mode-face '(:family "Comfortaa" :height 220))
   (buffer-face-mode))
 
 (add-hook 'eww-mode-hook 'my:buffer-face-mode-text)
