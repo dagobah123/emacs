798d15a154cb73387189d8a8628833dff9f4a886
Albert Likaj <dagobah1528@gmail.com>
2022-01-04 23:42:02 +0100

    Clean-up emacs.org

--- chiaroscuro-theme.el
+++ chiaroscuro-theme.el
@@ -619,7 +619,7 @@
   `(org-agenda-restriction-lock       ((t (:foreground ,text-1                                                                            ))))
   `(org-agenda-structure              ((t (:foreground ,text-1                                                                            ))))
   `(org-archived                      ((t (:foreground ,text-1  :background ,background-1 ))))
-  `(org-block                         ((t (:foreground ,text-1  :background ,background-5 ))))
+  `(org-block                         ((t (:foreground ,text-1  :background ,background-4 ))))
   `(org-block-begin-line              ((t (:foreground ,text-2  :background ,background-2                                               ))))
   `(org-block-end-line                ((t (:foreground ,text-2  :background ,background-2                                               ))))
   `(org-checkbox                      ((t (:foreground ,text-1  :background ,background-1 ))))
