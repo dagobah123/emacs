c8e2f3ed409fbe869677bea7594e96285b58133f
Albert Likaj <dagobah1528@gmail.com>
2022-04-18 19:27:00 +0200

    Update theme

---
+++ theme_default_dark.el
@@ -0,0 +1,50 @@
+;;; theme_default_dark --- common dark theme
+
+;;; Commentary:
+
+;;; Code:
+
+(setq green-1 "#00de00")
+(setq green-2 "#007700")
+(setq red-1 "#ff3333")
+(setq red-2 "#990000")
+(setq blue-1 "#1e90ff")
+(setq blue-2 "#222299")
+(setq yellow-1 "#ffd700")
+(setq yellow-2 "#cdad00")
+(setq violet-1 "#f974f9")
+(setq violet-2 "#43006d")
+(setq orange-1 "#ff7f00")
+(setq orange-2 "#cd6600")
+(setq gray-1 "#999999")
+(setq gray-2 "#666666")
+
+;mode line
+(setq text-buffer-id "#00de00")
+(setq background-buffer-id "#007700")
+(setq text-telephone-line-projectile "#00de00")
+(setq background-telephone-line-projectile "#007700")
+
+;match
+(setq text-match "#f974f9")
+(setq background-match "#43006d")
+
+;magit
+(setq text-diff-added "#b4ba5f")
+(setq background-diff-added "#2b550c")
+(setq text-diff-removed "#f17b6c")
+
+(setq text-magit-branch-local "#b4ba5f")
+(setq text-magit-branch-remote "#f17b6c")
+(setq background-magit-branch-local "#2b550c")
+(setq background-magit-branch-remote "#91423a")
+
+;ediff
+(setq background-diff-removed "#48241f")
+(setq background-current-diff-A "#994444")
+(setq background-current-diff-B "#226622")
+(setq background-current-diff-C "#887400")
+(setq background-fine-diff-A "#7d2323")
+(setq background-fine-diff-B "#124d12")
+(setq background-fine-diff-C "#726000")
+(setq background-ediff "#232723")
---
+++ theme_default_light.el
@@ -0,0 +1,50 @@
+;;; theme_default_light --- common light theme
+
+;;; Commentary:
+
+;;; Code:
+
+(setq green-1 "#00de00")
+(setq green-2 "#007700")
+(setq red-1 "#ff3333")
+(setq red-2 "#990000")
+(setq blue-1 "#222299")
+(setq blue-2 "#1e90ff")
+(setq yellow-1 "#ffd700")
+(setq yellow-2 "#cdad00")
+(setq violet-1 "#43006d")
+(setq violet-2 "#f974f9")
+(setq orange-1 "#ff7f00")
+(setq orange-2 "#cd6600")
+(setq gray-1 "#999999")
+(setq gray-2 "#666666")
+
+;mode line
+(setq text-buffer-id "#000000")
+(setq background-buffer-id "#ffff00")
+(setq text-telephone-line-projectile "#000000")
+(setq background-telephone-line-projectile "#00de00")
+
+;match
+(setq text-match "#000000")
+(setq background-match "#e4a1ff")
+
+;magit
+(setq text-diff-added "#229506")
+(setq background-diff-added "#9bff83")
+(setq text-diff-removed "#dc0f50")
+
+(setq text-magit-branch-local "#2b550c")
+(setq text-magit-branch-remote "#91423a")
+(setq background-magit-branch-local "#b4ba5f")
+(setq background-magit-branch-remote "#f17b6c")
+
+;ediff
+(setq background-diff-removed "#feacc7")
+(setq background-current-diff-A "#ffdddd")
+(setq background-current-diff-B "#ddffdd")
+(setq background-current-diff-C "#ffffaa")
+(setq background-fine-diff-A "#ffbbbb")
+(setq background-fine-diff-B "#aaffaa")
+(setq background-fine-diff-C "#ffff55")
+(setq background-ediff "#f7f7f7")
