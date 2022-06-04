aabceef80eaab1e712f82e1402af3ac9b0728b01
Albert Likaj <dagobah1528@gmail.com>
2022-06-05 00:14:36 +0200

    Update themes

--- theme_eclipse.el
+++ theme_eclipse.el
@@ -118,7 +118,7 @@
 (setq background-1 "#ffffff")
 (setq background-2 "#dddddd")
 (setq background-3 "#bbbbbb")
-(setq background-4 "#701654")
+(setq background-4 "#e8f2fe")
 (setq background-5 "#dddddd")
 (setq background-6 "#dddddd")
 (setq background-7 "#e8f2fe")
