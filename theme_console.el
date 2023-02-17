2eef4fcbab1e4a848106b52c0c53d8c1047b0e4a
Albert Likaj <dagobah1528@gmail.com>
2023-02-17 18:06:16 +0100

    Add console theme

---
+++ theme_console.el
@@ -0,0 +1,47 @@
+;;; theme_console --- console theme
+;;; Commentary:
+;;; Code:
+
+(defvar evil-emacs-1)
+(defvar evil-emacs-2)
+(defvar evil-normal-1)
+(defvar evil-normal-2)
+(defvar evil-insert-1)
+(defvar evil-insert-2)
+(defvar evil-replace-1)
+(defvar evil-replace-2)
+(defvar evil-replace-1)
+(defvar evil-replace-2)
+(defvar evil-visual-1)
+(defvar evil-visual-2)
+(defvar evil-motion-1)
+(defvar evil-motion-2)
+(defvar evil-operator-1)
+(defvar evil-operator-2)
+
+(defvar text-mode-line)
+(defvar text-mode-line-border)
+(defvar background-buffer-id-projectile)
+
+(setq evil-emacs-1 "#611481") ;violet
+(setq evil-emacs-2 "#f0cdff") ;violet
+(setq evil-normal-1 "#113e00") ;green
+(setq evil-normal-2 "#d6ffc7") ;green
+(setq evil-insert-1 "#850b0b") ;red
+(setq evil-insert-2 "#ffc8c8") ;red
+(setq evil-replace-1 "#d6ffc7") ;red
+(setq evil-replace-2 "#850b0b") ;red
+(setq evil-replace-1 "#7b2d06") ;orange
+(setq evil-replace-2 "#ffce82") ;orange
+(setq evil-visual-1 "#645900") ;yellow
+(setq evil-visual-2 "#fff190") ;yellow
+(setq evil-motion-1 "#1111ca") ;blue
+(setq evil-motion-2 "#bcdfff") ;blue
+(setq evil-operator-1 "#1111ca") ;blue
+(setq evil-operator-2 "#bcdfff") ;blue
+
+(setq text-mode-line "#000000")
+(setq text-mode-line-border "#000000")
+(setq background-buffer-id-projectile "#ffff00")
+
+;;; theme_console.el ends here
