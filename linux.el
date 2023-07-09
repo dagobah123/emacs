d24d18a8002798d9251c0c432375d80a672039ec
Albert Likaj <dagobah1528@gmail.com>
2023-07-09 16:13:43 +0200

    Update hydra

--- linux.el
+++ linux.el
@@ -46,45 +46,9 @@
 ;(add-hook 'lsp-mode-hook 'my:buffer-face-mode-mono)
 ;(add-hook 'lisp-mode-hook 'my:buffer-face-mode-mono)
 
-(defhydra hydra-zoom (global-map "<f2>")
-  "zoom"
-  ("g" text-scale-increase "in")
-  ("l" text-scale-decrease "out"))
-
-(defhydra hydra-buffer-menu (:color pink
-                             :hint nil)
-  "
-^Mark^             ^Unmark^           ^Actions^          ^Search
-^^^^^^^^-----------------------------------------------------------------                        (__)
-_m_: mark          _u_: unmark        _x_: execute       _R_: re-isearch                         (oo)
-_s_: save          _U_: unmark up     _b_: bury          _I_: isearch                      /------\\/
-_d_: delete        ^ ^                _g_: refresh       _O_: multi-occur                 / |    ||
-_D_: delete up     ^ ^                _T_: files only: % -28`Buffer-menu-files-only^^    *  /\\---/\\
-_~_: modified      ^ ^                ^ ^                ^^                                 ~~   ~~
-"
-  ("m" Buffer-menu-mark)
-  ("u" Buffer-menu-unmark)
-  ("U" Buffer-menu-backup-unmark)
-  ("d" Buffer-menu-delete)
-  ("D" Buffer-menu-delete-backwards)
-  ("s" Buffer-menu-save)
-  ("~" Buffer-menu-not-modified)
-  ("x" Buffer-menu-execute)
-  ("b" Buffer-menu-bury)
-  ("g" revert-buffer)
-  ("T" Buffer-menu-toggle-files-only)
-  ("O" Buffer-menu-multi-occur :color blue)
-  ("I" Buffer-menu-isearch-buffers :color blue)
-  ("R" Buffer-menu-isearch-buffers-regexp :color blue)
-  ("c" nil "cancel")
-  ("v" Buffer-menu-select "select" :color blue)
-  ("o" Buffer-menu-other-window "other-window" :color blue)
-  ("q" quit-window "quit" :color blue))
-
-(global-set-key (kbd "C-]") 'hydra-buffer-menu/body)
 
 
-(defhydra hydra-navigation (:hint nil)
+(defhydra hydra-navigation (:hint nil :color red)
 
   "
 Emacs
@@ -104,7 +68,7 @@ _f_: chiaro...el    ^ ^                    _w_: Eclipse TODO  ^ ^
   ("f" (find-file "~/source/emacs/chiaroscuro-theme.el"))
 
   ("g" (lambda () (interactive)
-         (shell-command "cd ~/.emacs.d ; cp -r /home/pi/source/emacs/* .")))
+         (shell-command "cd ~/.emacs.d ; cp -r ~/source/emacs/* .")))
   ("h" (my:open-and-eval-init-file))
   ("j" (toggle-frame-fullscreen))
 
@@ -112,7 +76,9 @@ _f_: chiaro...el    ^ ^                    _w_: Eclipse TODO  ^ ^
   ("l" (my:theme-up))
   ("q" (dired "/home/q/"))
   ("w" (dired "/home/w/"))
-  ("e" (dired "/home/e/")))
+  ("e" (dired "/home/e/"))
+
+  ("r" nil "Quit" :color blue))
 
 (global-set-key (kbd "C-;") 'hydra-navigation/body)
 
@@ -120,6 +86,7 @@ _f_: chiaro...el    ^ ^                    _w_: Eclipse TODO  ^ ^
   "Master Hydra"
   ("a" hydra-sub1/body "Sub-Hydra 1")
   ("b" hydra-sub2/body "Sub-Hydra 2")
+  ("c" hydra-navigation/body "Emacs")
   ("q" nil "Quit" :color red))
 
 (defhydra hydra-sub1 (:color red)
@@ -133,7 +100,7 @@ _f_: chiaro...el    ^ ^                    _w_: Eclipse TODO  ^ ^
   ("z" command3 "Command 3")
   ("q" nil "Quit" :color blue))
 
-(global-set-key (kbd "C-c m") 'hydra-master/body)
+(global-set-key (kbd "C-,") 'hydra-master/body)
 
 
 (defun my:open-and-eval-init-file ()
