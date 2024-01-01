811b3d23b6f6dae6dfc97476f6d3d0685e81b60d
Albert Likaj <dagobah1528@gmail.com>
2024-01-01 17:59:28 +0100

    Update neon theme

--- theme_neon.el
+++ theme_neon.el
@@ -11,7 +11,7 @@
 (setq background-magit-dark "#000000")
 (setq background-mode-line "#191919")
 (setq background-mode-line-active "#191919")
-(setq background-region "#dd0063")
+(setq background-region "#ef9d03")
 (setq text-highlight-1 "#66bb66")
 (setq text-highlight-2 "#ef9d03")
 (setq text-hl "#ef9d03")
