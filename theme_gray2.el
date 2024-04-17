7474dbaea29aeef13be9af5c6e53bbb399c2f2a0
Albert Likaj <dagobah1528@gmail.com>
2024-04-17 22:07:43 +0200

    Update

---
+++ theme_gray2.el
@@ -0,0 +1,35 @@
+;;; gray --- theme
+;;; Commentary:
+;;; Code:
+
+(load-file "~/.emacs.d/theme_variables.el")
+
+(setq background "#000000")
+(setq background-fringe "#0a0a0a")
+(setq background-hl "#222222")
+(setq background-low "#222222")
+(setq background-magit "#0a0a0a")
+(setq background-magit-dark "#000000")
+(setq background-mode-line "#0a0a0a")
+(setq background-mode-line-active "#0a0a0a")
+(setq background-region "#0a0a0a")
+(setq text-comment "#8264a7")
+(setq text-constant "#8264a7")
+(setq text-function "#25be7b")
+(setq text-highlight-1 "#00ff4e")
+(setq text-highlight-2 "#ffc73b")
+(setq text-highlight-3 "#00beff")
+(setq text-info "#dd7700")
+(setq text-keyword "#af5b59")
+(setq text-line-number "#666666")
+(setq text-low "#555555")
+(setq text-lower "#333333")
+(setq text-match "#000000")
+(setq text-mode-line "#af5b59")
+(setq text-normal "#888888")
+(setq text-preprocessor "#00ff00")
+(setq text-type "#5c7bba")
+(setq text-variable "#bbb14f")
+(setq text-whitespace "#ffffff")
+
+;;; theme_gray.el ends here
